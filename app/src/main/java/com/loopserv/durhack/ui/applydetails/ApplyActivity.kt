53aa500c6ca491eea829d70e71cf77c84d37fdb1
idraskovits <i.draskovits@ncl.ac.uk>
2019-11-23 18:02:28 +0000

    added apply screen, needs functionality

---
+++ app/src/main/java/com/loopserv/durhack/ui/applydetails/ApplyActivity.kt
@@ -0,0 +1,13 @@
+package com.loopserv.durhack.ui.applydetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loopserv.durhack.R
+
+class ApplyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply)
+    }
+}

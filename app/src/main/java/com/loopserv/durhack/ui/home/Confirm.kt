f9e558436f72bb551e99f20c2c5373a404112680
idraskovits <i.draskovits@ncl.ac.uk>
2019-11-23 23:57:43 +0000

    Merge branch 'master' of https://github.com/CUB3D/Durhack
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/loopserv/durhack/ui/home/Confirm.kt
@@ -0,0 +1,13 @@
+package com.loopserv.durhack.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loopserv.durhack.R
+
+class Confirm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_id)
+    }
+}
